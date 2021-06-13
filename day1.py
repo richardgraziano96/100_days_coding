43835f6ca6060623af18c26224a4e44161b29e7d
Richard Graziano <richard.graziano15@gmail.com>
2021-06-13 12:19:48 -0400

    first push

---
+++ day1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+city = input("What city did you grow up in?\n")
+
+petname = input("What is the name of a pet?\n")
+
+print("Your band name could be " + city + " " + petname)
