7103d5e5a03f96296baa7e195812e7c6e1eb31a0
Richard Graziano <richard.graziano15@gmail.com>
2021-08-01 15:13:30 -0400

    Started day 2, finished quiz and moved on to the next lesson

---
+++ Day 2/day2.py
@@ -0,0 +1,22 @@
+## Takes user input and places it into a variable "num_char" for later use
+num_char = len(input("What is your name? "))
+
+## Recalls num_char to a string for it to be printed later
+new_num_char = str(num_char)
+
+## Prints how many letters are in your name (or whatever work you input)
+print("Your name has " + new_num_char + " chars.")
+
+### ~~~~~~ Information under this line is commented out as they aren't needed. ~~~~~~
+## Checks the variable type and prints it
+# print(type(num_char))
+
+## Assigns float to a number 
+# a = float(123)
+## Prints the variable type
+# print(type(a))
+
+## Converts the string "100.5" to a float and adds it to the other int
+# print(70 + float("100.5"))
+## Literally prints the strings 70 and 100 as text
+# print(str(70) + str(100))
---
+++ Day 2/day2quiz1.py
@@ -0,0 +1,15 @@
+## Bolds the string to make more emphasis 
+class color:
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+while len(two_digit_number) > 2:
+    print("Error. Only " + color.BOLD + "TWO" + color.END + " digits are allowed.")
+    two_digit_number = input("Type a two digit number: ")
+####################################
+#Write your code below this line 👇
+
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
