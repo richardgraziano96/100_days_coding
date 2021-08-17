e2503ceed98c9960038cf6df0c12396f39dd92bb
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-08-17 19:31:05 -0400

    fixed trailing whitespace

--- Day 3/day3exercise3.py
+++ Day 3/day3exercise3.py
@@ -1,11 +1,11 @@
 year = int(input("Which year would you like to check? "))
 
 ## Code according to given flowchart/information
-if year % 400 == 0: 
+if year % 400 == 0:
     print("Leap year")
 elif year % 100 == 0:
     print("Not a leap year")
-elif year % 4 == 0: 
+elif year % 4 == 0:
     print("Leap Year")
-else: 
+else:
     print("Not a leap year")
