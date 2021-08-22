4579d8e0c382833346893de5342641e79f3c9c22
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-08-22 18:53:34 -0400

    whitespace

--- Day 5/day5exercise4.py
+++ Day 5/day5exercise4.py
@@ -2,7 +2,7 @@
 
 
 for numbers in range(1, 101):
-    if numbers % 3 == 0 and numbers % 5 == 0: 
+    if numbers % 3 == 0 and numbers % 5 == 0:
         print("FizzBuzz!")
     elif numbers % 3 == 0:
         print("Fizz")
