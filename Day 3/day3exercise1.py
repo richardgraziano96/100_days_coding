e58a582dadfc687ea15027782f73e19215f39fbb
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-08-21 22:14:03 -0400

    whitespace

--- Day 3/day3exercise1.py
+++ Day 3/day3exercise1.py
@@ -5,7 +5,7 @@ number = int(input("Which number do you want to check? "))
 #Write your code below this line 👇
 
 '''If your number is even like 8 you get 2 + 2 + 2 + 2 with NO remainder.
-while if you have a number like 9 you get 2 + 2 + 2 + 2 + 1 which gives you a 1 as 
+while if you have a number like 9 you get 2 + 2 + 2 + 2 + 1 which gives you a 1 as
 remainder. Thus we can see that 0 is even and 1 is odd. '''
 if number % 2 == 0:
     print("Your number is EVEN.")
