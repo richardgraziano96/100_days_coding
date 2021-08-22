7f9ca9b2b6d48f888196805dd8fc6c8548cef2dd
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-08-21 22:14:35 -0400

    whitespace

--- Day 3/day3exercise2.py
+++ Day 3/day3exercise2.py
@@ -1,2 +1,1 @@
-''' It seems that I have already gone above and beyond with day 2 quiz 2's BMI 
-calculator thus this is already implemented '''
+#It seems that I have already gone above and beyond with day 2 quiz 2's BMI calculator thus this is already implemented
