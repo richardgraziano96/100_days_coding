639c4065d80ea2a17b5f22d5de216634fea72329
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-08-22 18:55:11 -0400

    commented out
     the codacy keeps yelling at me

--- Day 5/day5exercise5.py
+++ Day 5/day5exercise5.py
@@ -1,5 +1,5 @@
 #Password Generator Project
-import random
+'''import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -29,4 +29,4 @@ print(code_holder)
 scrambling = list(code_holder)
 random.shuffle(scrambling)
 result = ''.join(scrambling)
-print(result)
+print(result)'''
