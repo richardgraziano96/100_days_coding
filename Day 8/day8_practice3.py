1b9f50a85ff1bb0f8b89c5eeccf0e00b6095d035
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-10-23 23:44:09 -0400

    Update day8_practice3.py

--- Day 8/day8_practice3.py
+++ Day 8/day8_practice3.py
@@ -15,8 +15,8 @@ def encrpyt(text, shift):
     print(f"The encoded text is {cipher_text}")
 
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    #e.g.
     #plain_text = "hello"
     #shift = 5
     #cipher_text = "mjqqt"
@@ -27,5 +27,5 @@ def encrpyt(text, shift):
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
 encrpyt(text, shift)
