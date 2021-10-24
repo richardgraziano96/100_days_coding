4005a5a4dbd8389a8b270caac75f04abc3176d69
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-10-23 23:43:30 -0400

    Update day8.py

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -1,5 +1,5 @@
-# Review: 
-# Create a function called greet(). 
+# Review:
+# Create a function called greet().
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
