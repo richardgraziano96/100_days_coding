1362a05f6e3adfa38d91e007b6a293e09b479beb
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-10-25 15:39:28 -0400

    Update 1.py

--- Day 11/1.py
+++ Day 11/1.py
@@ -7,8 +7,8 @@
 
 ############### Our Blackjack House Rules #####################
 
-## The deck is unlimited in size. 
-## There are no jokers. 
+## The deck is unlimited in size.
+## There are no jokers.
 ## The Jack/Queen/King all count as 10.
 ## The the Ace can count as 11 or 1.
 ## Use the following list as the deck of cards:
@@ -18,16 +18,16 @@
 
 ##################### Hints #####################
 
-#Hint 1: Go to this website and try out the Blackjack game: 
+#Hint 1: Go to this website and try out the Blackjack game:
 #   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
+#Then try out the completed Blackjack project here:
 #   http://blackjack-final.appbrewery.repl.run
 
-#Hint 2: Read this breakdown of program requirements: 
+#Hint 2: Read this breakdown of program requirements:
 #   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
 #Then try to create your own flowchart for the program.
 
-#Hint 3: Download and read this flow chart I've created: 
+#Hint 3: Download and read this flow chart I've created:
 #   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
@@ -41,8 +41,8 @@ def deal_card():
   card = random.choice(cards)
   return card
 
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input
+#and returns the score.
 #Look up the sum() function to help you do this.
 def calculate_score(cards):
   """Take a list of cards and return the score calculated from the cards"""
