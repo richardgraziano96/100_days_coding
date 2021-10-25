ec7319179bc4c8e46d90b3ade7bfa8b0407475b2
Richard Graziano <56696847+richardgraziano96@users.noreply.github.com>
2021-10-25 15:40:50 -0400

    Update day9_end.py

--- Day 09/day9_end.py
+++ Day 09/day9_end.py
@@ -9,7 +9,7 @@ def find_highest_bidder(bidding_record):
   # bidding_record = {"Angela": 123, "James": 321}
   for bidder in bidding_record:
     bid_amount = bidding_record[bidder]
-    if bid_amount > highest_bid: 
+    if bid_amount > highest_bid:
       highest_bid = bid_amount
       winner = bidder
   print(f"The winner is {winner} with a bid of ${highest_bid}")
